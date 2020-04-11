2cfa3dcceaa7f2f99fe91964c773f53095d1258d
Facundo Alderete <facualderete@gmail.com>
2020-04-11 16:03:38 -0300

    Basic REST service working

---
+++ src/main/java/com/example/restservice/model/Greeting.java
@@ -0,0 +1,24 @@
+package com.example.restservice.model;
+
+/**
+ * This application uses the Jackson JSON library to automatically marshal instances of type Greeting into JSON.
+ * Jackson is included by default by the web starter.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
